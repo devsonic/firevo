81a4625d633163ef79a0c4dc28870b6a74ab7449
seongsu.kim <seongsu.kim@nhn.com>
2017-04-01 15:09:50 +0900

    bootstrap 추가 기본 페이지 추가 (controller 및 main.html)

---
+++ web/src/main/java/com/firevo/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.firevo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/")
+@Controller
+public class MainController {
+    @RequestMapping("")
+    public String index(Model model) {
+        model.addAttribute("message", "hello firevo!");
+        return "main";
+    }
+}
